17d8fce4e3f498999f0e577e8469972bab0cf311
OngJunJieRP <19032110@myrp.edu.sg>
2020-07-21 16:23:30 +0800

    Done doLoanChromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -227,6 +227,19 @@ public class ResourceCentre {
 	
 	public static boolean doLoanChromebook(ArrayList<Chromebook> chromebookList, String tag, String dueDate) {
 		// write your code here
+		
+		boolean isLoaned = false;
+		
+		for (int i = 0; i < chromebookList.size(); i ++) { 
+			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag()) 
+					&& chromebookList.get(i).getIsAvailable() == true) {
+				
+				chromebookList.get(i).setIsAvailable(false);
+				chromebookList.get(i).setDueDate(dueDate);
+				
+				isLoaned = true;
+			}
+		}
 		return true;
 	}
 	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
