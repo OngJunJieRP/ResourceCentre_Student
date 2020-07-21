9f8b0294ff10e6bdaba09ac92352016436603fb1
OngJunJieRP <19032110@myrp.edu.sg>
2020-07-21 16:29:55 +0800

    Done returnChromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -300,6 +300,16 @@ public class ResourceCentre {
 	}
 	public static void returnChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here
+		ResourceCentre.viewAllChromebook(chromebookList);
+		String tag = Helper.readString("Enter asset tag > ");
+		Boolean isReturned = doReturnChromebook(chromebookList, tag);
+		
+		if (isReturned == false ) {
+			System.out.println("Invalid asset tag");
+		}
+		else {
+			System.out.println("Chromebook " + tag + " returned");
+		}
 		// write your code here
 	}
 
